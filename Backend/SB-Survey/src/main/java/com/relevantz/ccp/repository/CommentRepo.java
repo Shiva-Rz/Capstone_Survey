1e46495233d452d168de2ffb408ba0e6abe7c701
Shiva-Rz <142473031+Shiva-Rz@users.noreply.github.com>
2024-03-14 17:50:50 +0530

    Delete Backend/SB-Survey/src directory

--- Backend/SB-Survey/src/main/java/com/relevantz/ccp/repository/CommentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.relevantz.ccp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.relevantz.ccp.model.Comment;
-
-public interface CommentRepo extends CrudRepository<Comment, Long> {
-
-}
