4aade5daee0d45845ff74d64754d8a14bd69ac40
Purushothaman <91775559+Purushothaman1962@users.noreply.github.com>
2024-03-14 15:42:19 +0530

    Delete Backend/SB-Survey/src directory

--- Backend/SB-Survey/src/main/java/com/relevantz/ccp/repository/QuestionsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.relevantz.ccp.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.relevantz.ccp.model.Questions;
-
-public interface QuestionsRepo extends CrudRepository<Questions, Long> {
-
-}
