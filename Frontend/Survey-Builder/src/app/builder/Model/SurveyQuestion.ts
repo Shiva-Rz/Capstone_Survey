e921dd2f68fb43d4b9aa5f5afac25081727e1b4c
shajiBS24 <126977885+shajiBS24@users.noreply.github.com>
2024-03-15 10:16:37 +0530

    Delete Frontend/Survey-Builder directory

--- Frontend/Survey-Builder/src/app/builder/Model/SurveyQuestion.ts
+++
@@ -1,8 +0,0 @@
-export class SurveyQuestion {
-    constructor(
-      public type: string,
-      public question: string,
-      public answer: string,
-      public options: string[] = []
-    ) {}
-  }
