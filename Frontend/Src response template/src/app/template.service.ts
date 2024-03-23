eb99062cede1dc59c08ec1c0d0d7242840b68840
Swetha Sadhasivam <115160983+swethacse2002@users.noreply.github.com>
2024-03-23 17:54:56 +0530

    Update template.service.ts
    Comment Added !

--- Frontend/Src response template/src/app/template.service.ts
+++ Frontend/Src response template/src/app/template.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Survey } from './Model/Suvey';
 import { Region } from './Model/Region';
 import { Observable } from 'rxjs';
+import { Comment } from './Model/Comment';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,13 @@ export class TemplateService {
   getSurvey(region : Region):Observable<any>{
     return this.http.get(this.url +  "/getsurveyregion/"+region.regionId);
   }
+
+  // getComment():Observable<any>{
+  //   return this.http.get(this.url +  "/comment/");
+  // }
+
+  getComment(survey:Survey):Observable<Comment[]> {
+    return this.http.get<Comment[]>(this.url + "/comment/"+survey.surveyId);
+  }
+  
 }
