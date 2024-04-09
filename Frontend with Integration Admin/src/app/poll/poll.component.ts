f30ced4d962b6c4d018059b21492bfaad8f552dc
Purushothaman <91775559+Purushothaman1962@users.noreply.github.com>
2024-04-09 09:50:04 +0530

    src added for frontend.

---
+++ Frontend with Integration Admin/src/app/poll/poll.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poll',
+  templateUrl: './poll.component.html',
+  styleUrls: ['./poll.component.css']
+})
+export class PollComponent {
+
+}
